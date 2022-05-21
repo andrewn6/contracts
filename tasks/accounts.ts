0fefe4dae8e284b4014e9aa653f1bb165c9f2871
Andrew <andrewnijmeh1@gmail.com>
2022-05-21 17:29:11 -0400

    add first code

---
+++ tasks/accounts.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line node/no-unpublished-import
+import { task } from "hardhat/config";
+
+task("accounts", "Prints the list of accounts", async (_, { ethers }) => {
+	const accounts = await ethers.getSigners();
+
+	for (const account of accounts) {
+		console.log(account.address);
+	}
+});
